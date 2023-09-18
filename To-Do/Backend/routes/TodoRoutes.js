f77fc01fb3466e8371435d0fc94378b1a1f9c128
kaur-rajdeep <kaurd3535@gmail.com>
2023-09-18 15:22:16 +0530

    added the backend and the frontend

--- To-Do/Backend/routes/TodoRoutes.js
+++ To-Do/Backend/routes/TodoRoutes.js
@@ -1,9 +1,15 @@
 //nodeMON
+
 const { Router } = require("express");
+const { getToDo } = require("../controller/TodoController");
 const router = Router();
-// router.get("/", getToDo);
-router.get("/", (req, res) => {
-  res.json({ message: "Hi there!" });
-});
+
+// const {model} = require("mongoose")
+
+router.get("/", getToDo);
+
+// router.get("/", (req, res) => {
+//   res.json({ message: "Hi there!" });
+// });
 
 module.exports = router;
--- To-Do/Backend/server.js
+++ To-Do/Backend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routes/TodoRoutes");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.port || 27017;
-console.log(process.env.MONGODB_URL);
-
+app.use(express);
+app.use(cors());
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected To Mongo"))
